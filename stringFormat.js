572ca69eef1b683c83e0abb26fd606a7db820417
minseong <37841755+gagreen@users.noreply.github.com>
2021-08-12 11:01:58 +0900

    내부 함수 매개변수 추가
    내부 함수 안의 this는 함수 자체를 가리키기 떄문에 prototype.format에서 내부 함수로 this를 넘겨준다.

--- stringFormat.js
+++ stringFormat.js
@@ -1,28 +1,28 @@
 if(!String.prototype.format) {
-    // 문자열.format(JSON 객체)
-    // ex) "is Success: ${success}".format({success: "yes"});
-    // ex) "${0}, ${1}".format('aaa', 'bbb');
-    String.prototype.format = function () {
-        function formatForObject(args){
-            var formatted = this;
-            var obj = args[0];
-            for(var keyword of Object.getOwnPropertyNames(obj)) {
-                formatted = formatted.replace("${"+ keyword + "}", obj[keyword]);
-            }
-        }
-        function formatForArguments(args){
-            var formatted = this;
-            for( var arg in args ) {
-                formatted = formatted.replace("{" + arg + "}", arguments[arg]);
-            }
-            return formatted;
-        }
+	// 문자열.format(JSON 객체)
+	// ex) "is Success: ${success}".format({success: "yes"});
+	// ex) "${0}, ${1}".format('aaa', 'bbb');
+	String.prototype.format = function () {
+		function formatForObject(str, args){
+			var formatted = str;
+			var obj = args[0];
+			for(var keyword of Object.getOwnPropertyNames(obj)) {
+				formatted = formatted.replace("${"+ keyword + "}", obj[keyword]);
+			}
+		}
+		function formatForArguments(str, args){
+			var formatted = str;
+			for( var arg in args ) {
+				formatted = formatted.replace("{" + arg + "}", arguments[arg]);
+			}
+			return formatted;
+		}
 
-        if(typeof arguments[0] === "object") {
-            return formatForObject(arguments);
-        }
-        else {
-            return formatForArguments(arguments);
-        }
-    }
+		if(typeof arguments[0] === "object") {
+			return formatForObject(this, arguments);
+		}
+		else {
+			return formatForArguments(this, arguments);
+		}
+	}
 }
